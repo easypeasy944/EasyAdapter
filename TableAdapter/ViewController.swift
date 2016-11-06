a443821f23736c0eab3a616f535073c4f2d58d1b
Aynur Galiev <aynurgaliev@Mac-mini-Aynur.local>
2016-11-06 18:59:57 +0300

    Initial Commit

---
+++ TableAdapter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableAdapter
+//
+//  Created by Aynur Galiev on 6.ноября.2016.
+//  Copyright © 2016 Aynur Galiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
